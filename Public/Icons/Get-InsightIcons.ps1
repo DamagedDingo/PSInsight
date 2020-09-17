966307b9c25275225d7f8c88cac14bbc6164c81a
AllElbows <gary.ryan.smith@gmail.com>
2020-09-17 14:13:57 +1000

    Add files via upload

--- Public/Icons/Get-InsightIcons.ps1
+++ Public/Icons/Get-InsightIcons.ps1
@@ -3,19 +3,57 @@
 .SYNOPSIS
 Get avalible Insight Icons and there corrisponding ID.
 .EXAMPLE
-$Headers = Get-InsightIcons -InsightApiKey $InsightApiKey
+$Headers = 
 .OUTPUTS
 Something something.
 .LINK
-https://documentation.mindville.com/display/ICV811/Icons+-+REST
+
 
 #>
 
+<#
+
+.SYNOPSIS
+Resource to find global Icons in Insight.
+
+.DESCRIPTION
+Resource to find global Icons in Insight.
+
+.PARAMETER Full
+If full switch is used then property url16 is returned
+
+.PARAMETER InsightApiKey
+The Api key.
+
+.OUTPUTS
+If switch 'Full' is used 
+id name                 url16                                                                                                               
+ -- ----                 -----                                                                                                               
+  1 3D Printer           /rest/insight/1.0/icon/1/icon.png?size=16&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjb20ucmlhZGFsYWJzLm...
+  2 AV Receiver          /rest/insight/1.0/icon/2/icon.png?size=16&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjb20ucmlhZGFsYWJzLm...
+
+if switch 'Full' is not used
+id name                
+ -- ----                
+  1 3D Printer          
+  2 AV Receiver         
+  3 Active Directory    
+  4 Administrative Tools
+  5 Agreement
+
+.LINK
+https://documentation.mindville.com/display/ICV811/Icons+-+REST
+
+.EXAMPLE
+Get-InsightIcons -InsightApiKey $InsightApiKey
+
+#>
 
 function Get-InsightIcons {
     [CmdletBinding()]
     param (
         [ValidateNotNullOrEmpty()]
+        [Alias('ApiKey')]
         [string]$InsightApiKey,
 
         [switch]$Full = $False
