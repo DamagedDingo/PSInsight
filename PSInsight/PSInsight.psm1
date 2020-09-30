19fdbfb7a89506bc2234c665c53953843be7ef76
AllElbows <gary.ryan.smith@gmail.com>
2020-09-30 15:33:12 +1000

    appveyor and build added.

--- PSInsight/PSInsight.psm1
+++ PSInsight/PSInsight.psm1
@@ -1,24 +1,11 @@
 $Public  = @(Get-ChildItem -Path "$PSScriptRoot\Public\" -include '*.ps1' -recurse -ErrorAction SilentlyContinue)
 $Private = @(Get-ChildItem -Path "$PSScriptRoot\Private\" -include '*.ps1' -recurse -ErrorAction SilentlyContinue)
 
-foreach ($ps1 in @($Public + $Private)) {
+foreach ($ImportedFunction in @($Public + $Private)) {
     try {
-        . $ps1.fullname
+        . $ImportedFunction.fullname
     } catch {
-        Write-Error -Message "Failed to import function $($ps1.fullname): $_"
-    }
-}
-
-# Load up dependency modules
-foreach($ps1 in $Private)
-{
-    Try
-    {
-        Import-Module $ps1 -ErrorAction Stop
-    }
-    Catch
-    {
-        Write-Error "Failed to import module $ps1`: $_"
+        Write-Error -Message "Failed to import function $($ImportedFunction.fullname): $_"
     }
 }
 
