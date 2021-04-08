f9f8f68d8fd8b014fd98a33fd4315678c1e8100d
Gary Smith <gary.ryan.@gmail.com>
2021-04-08 13:15:19 +1000

    !Deploy!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 Add comment under [Unreleased] and commit with !Deploy!
 
 ## [Unreleased]
+### Added
 - Move Test-Module from Private to public\build
 - Update ReadMe.MD
 
--- Examples/Objects/Zoom Rooms/Navitas.Insight.ZoomRoom.Importer.ps1
+++ Examples/Objects/Zoom Rooms/Navitas.Insight.ZoomRoom.Importer.ps1
@@ -1,35 +1,5 @@
 # Script contains the required config for syncing Zoom with Insight
 
-#region Required-Modules
-function Test-Module ($m) {
-
-    # If module is imported say that and do nothing
-    if (Get-Module | Where-Object {$_.Name -eq $m}) {
-        write-host "Module $m is already imported."
-    }
-    else {
-
-        # If module is not imported, but available on disk then import
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
-            Import-Module $m -Verbose
-        }
-        else {
-
-            # If module is not imported, not available on disk, but is in online gallery then install and import
-            if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
-                Install-Module -Name $m -Force -Verbose -Scope CurrentUser
-                Import-Module $m -Verbose
-            }
-            else {
-
-                # If module is not imported, not available and not in online gallery then abort
-                write-host "Module $m not imported, not available and not in online gallery, exiting."
-                EXIT 1
-            }
-        }
-    }
-}
-
 Test-Module PSZoom
 Test-Module PSInsight
 Test-Module Join-Object
