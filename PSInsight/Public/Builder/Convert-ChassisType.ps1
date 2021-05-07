d612212dc2194b5535cb4e96eb4c930f2d3c1fb9
Gary Smith <gary.ryan.@gmail.com>
2021-05-07 15:41:15 +1000

    !Deploy!

--- PSInsight/Public/Builder/Convert-ChassisType.ps1
+++ PSInsight/Public/Builder/Convert-ChassisType.ps1
@@ -2,153 +2,155 @@ function Convert-ChassisType {
     param (
         [ValidateNotNullOrEmpty()]
         [Parameter(Mandatory = $true)]
-        [int]$ChassisID
+        [string]$ChassisID
     )
 
     class Chassis {
         [string]$Type
-        [int]$ID
+        [string]$ID
         [string]$Category
     }
+    
     
     $Chassis = [Chassis]::new()
     
     switch ($ChassisID) {
-        1 { 
+        "1" { 
             $Chassis.Type = 'Other'
             $Chassis.ID = $_
             $Chassis.Category = 'Unknown'
         }
-        2 {  
-            $Chassis.Type = 'Unknown'
-            $Chassis.ID = $_
-            $Chassis.Category = 'Unknown'
-        }
-        3 {
+        "3" {
             $Chassis.Type = 'Desktop'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        4 { 
+        "4" { 
             $Chassis.Type = 'Low Profile Desktop'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        5 { 
+        "5" { 
             $Chassis.Type = 'Pizza Box'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        6 { 
+        "6" { 
             $Chassis.Type = 'Mini Tower'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        7 { 
+        "7" { 
             $Chassis.Type = 'Tower'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        8 { 
+        "8" { 
             $Chassis.Type = 'Portable'
             $Chassis.ID = $_
             $Chassis.Category = 'Laptop'
         }
-        9 { 
+        "9" { 
             $Chassis.Type = 'Laptop'
             $Chassis.ID = $_
             $Chassis.Category = 'Laptop'
         }
-        10 { 
+        "10" { 
             $Chassis.Type = 'Notebook'
             $Chassis.ID = $_
             $Chassis.Category = 'Laptop'
         }
-        11 { 
+        "11" { 
             $Chassis.Type = 'Hand Held'
             $Chassis.ID = $_
             $Chassis.Category = 'Tablet'
         }
-        12 { 
+        "12" { 
             $Chassis.Type = 'Docking Station'
             $Chassis.ID = $_
             $Chassis.Category = 'Dock'
         }
-        13 { 
+        "13" { 
             $Chassis.Type = 'All in One'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        14 { 
+        "14" { 
             $Chassis.Type = 'Sub Notebook'
             $Chassis.ID = $_
             $Chassis.Category = 'Laptop'
         }
-        15 { 
+        "15" { 
             $Chassis.Type = 'Space-Saving'
             $Chassis.ID = $_
             $Chassis.Category = 'Server'
         }
-        16 { 
+        "16" { 
             $Chassis.Type = 'Lunch Box'
             $Chassis.ID = $_
             $Chassis.Category = 'Server'
         }
-        17 { 
+        "17" { 
             $Chassis.Type = 'Main System Chassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Server'
         }
-        18 { 
+        "18" { 
             $Chassis.Type = 'Expansion Chassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        19 { 
+        "19" { 
             $Chassis.Type = 'SubChassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Unknown'
         }
-        20 { 
+        "20" { 
             $Chassis.Type = 'Bus Expansion Chassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Server'
         }
-        21 { 
+        "21" { 
             $Chassis.Type = 'Peripheral Chassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Unknown'
         }
-        22 { 
+        "22" { 
             $Chassis.Type = 'Storage Chassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Unknown'
         }
-        23 { 
+        "23" { 
             $Chassis.Type = 'Rack Mount Chassis'
             $Chassis.ID = $_
             $Chassis.Category = 'Server'
         }
-        24 { 
+        "24" { 
             $Chassis.Type = 'Sealed-Case PC'
             $Chassis.ID = $_
             $Chassis.Category = 'Desktop'
         }
-        30 { 
+        "30" { 
             $Chassis.Type = 'Tablet'
             $Chassis.ID = $_
             $Chassis.Category = 'Tablet'
         }
-        31 { 
+        "31" { 
             $Chassis.Type = 'Convertible'
             $Chassis.ID = $_
             $Chassis.Category = 'Tablet'
         }
-        32 { 
+        "32" { 
             $Chassis.Type = 'Detachable'
             $Chassis.ID = $_
             $Chassis.Category = 'Tablet'
         }
+        Default {  
+            $Chassis.Type = 'Unknown'
+            $Chassis.ID = $_
+            $Chassis.Category = 'Unknown'
+        }
     }
     return $Chassis
 }
+
